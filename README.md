4383180f6ca05579dfd6acb3e8a3deda1f0fd9bd
Cristofer Cruz <cristofer.cruz@gmail.com>
2020-08-31 19:03:29 -0700

    Allow bitmap image as source, convert to tile data using img2gb. Fix error with unique tile limit

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 
 Python script to replace the built-in frames in the Game Boy Camera rom with your own!
 
+## Requisites
+This script requires the following Python libraries:
+```
+python -m pip install --upgrade Pillow
+python -m pip install --upgrade img2gb
+```
+
 ```
 usage: gbc-fr.py [-h] -mode {copy,inject} [-src-rom src.gb]
-                 [-src-frame [1-18]] [-src-image frame.bin] -dst-rom dest.gb
+                 [-src-frame [1-18]] [-src-image frame.png] -dst-rom dest.gb
                  -dst-frame [1-18]
 ```
 
@@ -17,28 +24,29 @@ Copy allows taking frame data from one rom file and pasting into another. You sp
 python gbc-fr.py -mode copy -src-rom pocketcam-jp.gb -src-frame 2 -dst-rom gameboycam-intl.gb -dst-frame 7
 ```
 
-Inject allows using a completely new image to replace an existing frame. You specify the source image in (2bpp binary tile format) as well as the target rom and target frame.
+Inject allows using a completely new image to replace an existing frame. You can specify the source image as a .png, .bmp and it will be converted to tile data or you can provide already formatted tile data as .bin. You will also specify the target rom and target frame.
 
 **Inject Example**: Load tile data from supplied image onto the International rom, replacing frame 18.
 ```
-python gbc-fr.py -mode inject -src-image cameraclub.bin -dst-rom gameboycam-intl.gb -dst-frame 18
+python gbc-fr.py -mode inject -src-image cameraclub.png -dst-rom gameboycam-intl.gb -dst-frame 18
 ```
 
 ## Designing your frame image
-Game Boy Camera frames can use up to 96 unique tiles but a frame is made up of 136 tiles so you will need to re-use or pattern some tiles. When designing your frame, you can show a grid to be aware of how many unique tiles you've used up. The script will ignore unique tiles after hitting the 96 tile limit and will re-use the first tile. The example below uses just 46 unique tiles and a re-used black tile for the rest of the frame.
+Game Boy Camera frames can use up to 96 unique tiles but a frame is made up of 136 tiles so you will need to re-use or pattern some tiles. When designing your frame, you can show a grid to be aware of how many unique tiles you've used up. The script will ignore unique tiles after hitting the 96 tile limit and will re-use the last tile. The example below uses just 46 unique tiles and a re-used black tile for the rest of the frame.
 
 ![Designing with grid](docs/frame-unique-tiles.png)
 
 ## Saving your frame image
-Make sure to save your image reduced down to 4 colors, this is necessary to convert to 2bpp Game Boy tile format.
+Make sure to save your image reduced down to 4 colors, this is necessary to convert to 2bpp Game Boy tile format well. **Be sure that your 4 shades have contrast or the converted result will appear with less than 4 colors.**
 ![Reduced color png](docs/reduced-colors.png)
 
 ## Converting your frame image
-Since this script accepts already tile formatted images, you will need to use a utility like [Pic2Tiles](http://www.budmelvin.com/dev/index.html) to do the conversion. Pic2Tiles accepts 4 color paletted images and allows saving as binary 2bpp format. Choose **binary** as the format in the application window. *Hoping to build tile conversion into this script later and eliminate this step.*
+This script uses img2gb library to convert .png and .bmp source images to tile data but if you prefer to do the conversion yourself for better control over the result, you can do so using a utility like [Pic2Tiles](http://www.budmelvin.com/dev/index.html)
 ![Pic2Tiles](docs/pic2tiles.png)
 
 ## Usage
 Your modified ROM will load into emulators but may show a warning about incorrect checksum. Eventually, your rom can be loaded onto a custom flashable Game Boy Camera cartridge once it becomes available.
 
 ## Credits
-Thanks to @jkbenaim for his [gbcamextract](https://github.com/jkbenaim/gbcamextract) program which helped to figure out the frame data and tile map addresses. 
+Thanks to @jkbenaim for their [gbcamextract](https://github.com/jkbenaim/gbcamextract) program which helped to figure out the frame data and tile map addresses.
+Thanks to @flozz for their [img2gb](https://github.com/flozz/img2gb) library which provides tile conversion for this script.
--- gbc-fr.py
+++ gbc-fr.py
@@ -1,5 +1,8 @@
 import argparse
 import binascii
+from img2gb import GBTileset
+from PIL import Image
+
 from argparse import RawTextHelpFormatter
 from argparse import RawDescriptionHelpFormatter
 
@@ -7,7 +10,7 @@ parser = argparse.ArgumentParser(description='Tool to modify standard photo fram
 parser.add_argument('-mode', required=True, choices=['copy', 'inject'], default='inject', help='inject or copy')
 parser.add_argument('-src-rom', metavar='src.gb', help='source rom in copy mode')
 parser.add_argument('-src-frame', metavar='[1-18]', choices=range(1,19), type=int, help='frame number [1-18] from source rom in copy mode')
-parser.add_argument('-src-image', metavar='frame.bin', help='source image in inject mode')
+parser.add_argument('-src-image', metavar='frame.png', help='source image in inject mode (.png, .bmp or already formatted tile data .bin)')
 parser.add_argument('-dst-rom', required=True, metavar='dest.gb', help='destination rom file')
 parser.add_argument('-dst-frame', required=True, metavar='[1-18]', choices=range(1,19), type=int, help='destination frame number [1-18]')
 args = parser.parse_args()
@@ -26,12 +29,10 @@ def frame_copy(source_rom, source_frame, destination_rom, destination_frame):
 			destinationRom.seek(850296+16384+1672*(destination_frame-9))
 		destinationRom.write(frameData)
 		destinationRom.close()
-	except Error as e:
-		print("error: {}".format( e ))
 	finally:
 		print('destination rom modified, frame copied\n')
 
-def frame_inject(source_image, destination_rom, destination_frame):
+def frame_inject(source_image, destination_rom, destination_frame, convert_2bpp):
 	try:
 		# iterate through source image, find unique tiles and store list of tiles as byte arrays
 		frameTiles = []
@@ -42,43 +43,79 @@ def frame_inject(source_image, destination_rom, destination_frame):
 		tileBytes = 16
 		tbTilePositions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,321,322,323,324,325,326,327,328,329,330,331,332,333,334,335,336,337,338,339,340,341,342,343,344,345,346,347,348,349,350,351,352,353,354,355,356,357,358,359,360]
 		lrTilePositions = [41,42,61,62,81,82,101,102,121,122,141,142,161,162,181,182,201,202,221,222,241,242,261,262,281,282,301,302,59,60,79,80,99,100,119,120,139,140,159,160,179,180,199,200,219,220,239,240,259,260,279,280,299,300,319,320]
-		sourceImage = open(source_image, "rb")
-		sourceImage.seek(0)
-		# read source image, one tile at a time
-		tile = sourceImage.read(tileBytes)
-		while tile:
-			# check if tile position is part of the frame
-			if currentTile in tbTilePositions or currentTile in lrTilePositions:
-				# check if it's a new/unique tile
-				if tile not in frameTiles:
-					# max of 96 unique tiles per frame
-					if uniqueTileIndex < 97:
-						# push unique tile index to tile map
-						if currentTile in tbTilePositions:
-							tbMap.append(uniqueTileIndex)
-						elif currentTile in lrTilePositions:
-							lrMap.append(uniqueTileIndex)
-						# push tile to frame data
-						frameTiles.append(tile)
-						uniqueTileIndex+=1
-					else:
-						# once tile limit is reached, re-use first tile
-						if currentTile in tbTilePositions:
-							tbMap.append(1)
-						elif currentTile in lrTilePositions:
-							lrMap.append(1)
-				else:
-					# push re-used tile index  to tile map
-					for i in range(len(frameTiles)):
-						if frameTiles[i] == tile:
+
+		if convert_2bpp:
+			image = Image.open(source_image)
+			sourceImage = GBTileset.from_image(image).tiles
+			for aTile in sourceImage:
+				tile = bytearray.fromhex(aTile.to_hex_string())
+				# check if tile position is part of the frame
+				if currentTile in tbTilePositions or currentTile in lrTilePositions:
+					# check if it's a new/unique tile
+					if tile not in frameTiles:
+						# max of 96 unique tiles per frame
+						if uniqueTileIndex < 96:
+							# push unique tile index to tile map
 							if currentTile in tbTilePositions:
-								tbMap.append(i)
+								tbMap.append(uniqueTileIndex)
 							elif currentTile in lrTilePositions:
-								lrMap.append(i)
-			# read next tile
+								lrMap.append(uniqueTileIndex)
+							# push tile to frame data
+							frameTiles.append(tile)
+							uniqueTileIndex+=1
+						else:
+							# once tile limit is reached, re-use first tile
+							if currentTile in tbTilePositions:
+								tbMap.append(95)
+							elif currentTile in lrTilePositions:
+								lrMap.append(95)
+					else:
+						# push re-used tile index  to tile map
+						for i in range(len(frameTiles)):
+							if frameTiles[i] == tile:
+								if currentTile in tbTilePositions:
+									tbMap.append(i)
+								elif currentTile in lrTilePositions:
+									lrMap.append(i)
+				currentTile+=1
+		else:
+			sourceImage = open(source_image, "rb")
+			sourceImage.seek(0)
+			# read source image, one tile at a time
 			tile = sourceImage.read(tileBytes)
-			currentTile+=1
-		sourceImage.close()
+			while tile:
+				# check if tile position is part of the frame
+				if currentTile in tbTilePositions or currentTile in lrTilePositions:
+					# check if it's a new/unique tile
+					if tile not in frameTiles:
+						# max of 96 unique tiles per frame
+						if uniqueTileIndex < 96:
+							# push unique tile index to tile map
+							if currentTile in tbTilePositions:
+								tbMap.append(uniqueTileIndex)
+							elif currentTile in lrTilePositions:
+								lrMap.append(uniqueTileIndex)
+							# push tile to frame data
+							frameTiles.append(tile)
+							uniqueTileIndex+=1
+						else:
+							# once tile limit is reached, re-use first tile
+							if currentTile in tbTilePositions:
+								tbMap.append(95)
+							elif currentTile in lrTilePositions:
+								lrMap.append(95)
+					else:
+						# push re-used tile index  to tile map
+						for i in range(len(frameTiles)):
+							if frameTiles[i] == tile:
+								if currentTile in tbTilePositions:
+									tbMap.append(i)
+								elif currentTile in lrTilePositions:
+									lrMap.append(i)
+				# read next tile
+				tile = sourceImage.read(tileBytes)
+				currentTile+=1
+			sourceImage.close()
 
 		# combine tile maps
 		tileMap = tbMap+lrMap
@@ -100,12 +137,15 @@ def frame_inject(source_image, destination_rom, destination_frame):
 			destinationRom.seek(850296+16384+1672*(destination_frame-9))
 		destinationRom.write(frameData)
 		destinationRom.close()
-	except Error as e:
-		print("error: {}".format( e ))
 	finally:
 		print('destination rom modified, frame injected\n')
 
 if args.mode == "copy":
 	frame_copy(args.src_rom, args.src_frame, args.dst_rom, args.dst_frame)
 else:
-	frame_inject(args.src_image, args.dst_rom, args.dst_frame)
+	if args.src_image.endswith('bin'):
+		frame_inject(args.src_image, args.dst_rom, args.dst_frame, False)
+	elif args.src_image.endswith('png') or args.src_image.endswith('bmp'):
+		frame_inject(args.src_image, args.dst_rom, args.dst_frame, True)
+	else:
+		print('-src-image can be .png, .bmp or already converted .bin (2bpp)')
